4adfbfc5a20bc0dbb2d965cadf1faa2f6f78e88d
Vivek Atreya <atreya.vivek9@gmail.com>
2024-11-27 16:34:28 +0530

    Added functionality for custom delimiters

--- src/StringCalculator.js
+++ src/StringCalculator.js
@@ -1,8 +1,17 @@
 class StringCalculator {
   add(numbers) {
     if (!numbers) return 0;
+
+    let delimiters = [",", "\n"];
+    if (numbers.startsWith("//")) {
+      const delimiterEnd = numbers.indexOf("\n");
+      const customDelimiter = numbers.substring(2, delimiterEnd);
+      delimiters.push(customDelimiter);
+      numbers = numbers.substring(delimiterEnd + 1);
+    }
+
     return numbers
-      .split(/,|\n/)
+      .split(new RegExp(`[${delimiters.join("")}]`))
       .map(Number)
       .reduce((sum, n) => sum + n, 0);
   }
