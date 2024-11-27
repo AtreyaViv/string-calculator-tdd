1b2f35452cf2e96ff65b7228a0d150d6d883cab3
Vivek Atreya <atreya.vivek9@gmail.com>
2024-11-27 15:50:41 +0530

    Testcase added to handle single number input

--- tests/StringCalculator.test.js
+++ tests/StringCalculator.test.js
@@ -1,8 +1,12 @@
-const StringCalculator = require('../src/StringCalculator');
+const StringCalculator = require("../src/StringCalculator");
 
-describe('StringCalculator', () => {
-  test('returns 0 for an empty string', () => {
+describe("StringCalculator", () => {
+  test("returns 0 for an empty string", () => {
     const calculator = new StringCalculator();
     expect(calculator.add("")).toBe(0);
+  });
+  test("returns the number itself for a single number", () => {
+    const calculator = new StringCalculator();
+    expect(calculator.add("5")).toBe(5);
   });
 });
